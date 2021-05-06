bd97b00674f040a4844820bccfbc4bebdf1db052
Allandouglaz <allanziinnhoo@gmail.com>
2021-05-06 11:27:45 -0300

    Basic static Components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/allandouglaz" target="_blank" rel="noreferrer">Allan Douglas</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+            Evento promovido pela escola DevSuperior: <a href="https://www.instagram.com/allandouglaaz/" target="_blank" rel="noreferrer">@allandouglaaz.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;

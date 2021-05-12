be023aede0c0fba77c3c3a33669ba570a2f58c36
Allandouglaz <allanziinnhoo@gmail.com>
2021-05-12 11:31:38 -0300

    Pagination

--- backend/dsvendas/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
+++ backend/dsvendas/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
@@ -2,7 +2,6 @@ package com.devsuperior.dsvendas.entities;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -44,7 +43,6 @@ public class Seller {
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public List<Sale> getSales() {
 		return sales;
 	}
